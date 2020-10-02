d4de3b3eaa3ff0c5b24fd9bd415ef31e2b2fa486
Andreas ERLANDSSON <10392013+CitedCub@users.noreply.github.com>
2020-10-02 11:49:54 +0200

    Add welcome message to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import "../App.css";
 function Header() {
   return (
     <div className="Header">
+      <div>Welcome!</div>
       <img src="images/wired-brain-coffee-logo.png" alt="Wired Brain" />
     </div>
   );
